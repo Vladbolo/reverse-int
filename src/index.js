7353965e5ece972715e2103a00a99b58ac70fd84
Vladbolo <bolo19514@gmail.com>
2022-06-22 16:50:54 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = String(Math.abs(n));
+    let newLength
+     console.log(str.split('').reverse().join(''))
+    return newLength = (str.split('').reverse().join(""))
+      
+  }
+
